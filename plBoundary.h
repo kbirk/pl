132e7b54fe70f0be25dec5539f570b01b11f0794
Thomas Vaughan <thomas.vaughan@queensu.ca>
2013-07-03 14:35:05 -0400

    Added comment to plBoundary.h about point direction/orientation.

--- plBoundary.h
+++ plBoundary.h
@@ -23,7 +23,7 @@ class plBoundary : public plRenderable
         plVector3 getAverageNormal() const;
         PLuint    size() const;
          
-        const plSeq<plVector3> &points () const { return _points;  }
+        const plSeq<plVector3> &points () const { return _points;  } // always in counterclockwise direction
         const plSeq<plVector3> &normals() const { return _normals; }
         
         const plVector3        &points ( PLuint index ) const { return _points[index];  }
