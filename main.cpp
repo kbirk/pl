6726c740fb50e602ff5b1f03e8e36534699d1621
Thomas Vaughan <thomas.vaughan@queensu.ca>
2013-10-16 15:18:05 -0400

    Restoring main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,8 @@
 // PLANNER
 #include "PlannerWindow.h"
-#include "plMeshIntersector.h"
 
 int main( int argc, char **argv )
 {   
-    plSeq<plTriangle> inputMesh1, inputMesh2;
-    plSTL::importFile(inputMesh1,"testMeshes/hiressphere.stl");
-    plSTL::importFile(inputMesh2,"testMeshes/sphere1.stl");
-    //plSTL::importFile(inputMesh1,"output/badInput3/badInput1.stl");
-    //plSTL::importFile(inputMesh2,"output/badInput3/badInput2.stl");
-
-    plSeq<plTriangle> output;
-
-    plMeshIntersector intersector;
-    intersector.importTriSeq(inputMesh1,0,PL_LOGGER_LEVEL_WARNING,0);
-    intersector.importTriSeq(inputMesh2,1,PL_LOGGER_LEVEL_WARNING,0);
-    intersector.intersect(output,PL_LOGGER_LEVEL_DEBUG,0);
-    plSTL::exportFileBinary(output,"output/output.stl");
-    /*
     // check command line argument count
     if (argc < 2)
     {
@@ -35,6 +20,6 @@ int main( int argc, char **argv )
     PlannerWindow window( 0, 0, 1400, 1050, "Planner", argc, argv); 
 
     // Run OpenGL
-    glutMainLoop();*/
+    glutMainLoop();
     
 }
