0bab5cb739f7a84445f17860857dac4a38758ea8
Matt Slonetsky-arges12.04 <slonetsky@cs.queensu.ca>
2013-11-23 17:03:32 -0500

    Added some files that it seems were ommitted from last night's commits.

---
+++ plArthroCamTechnique.cpp
@@ -0,0 +1,55 @@
+#include "plArthroCamTechnique.h"
+
+plArthroCamTechnique::plArthroCamTechnique()
+{
+}      
+
+
+void plArthroCamTechnique::render( const std::set< plRenderComponent >& componentSet ) const
+{
+    const std::shared_ptr< plFBO >&    fbo    = plRenderResources::fbos( PL_MAIN_FBO );
+    const std::shared_ptr< plShader >& shader = plRenderResources::shaders( PL_ARTHRO_CAM_SHADER );
+
+    // set initial rendering state
+    _initState();
+
+    // bind fbo
+    fbo->bind();
+    // bind shader
+    shader->bind();
+
+    // cset viewport
+    glViewport( 0, 0, plWindow::viewportWidth(), plWindow::viewportHeight() );
+
+    glDisable( GL_DEPTH_TEST );
+    glDepthMask( false );
+
+    // draw main render components
+    for ( const plRenderComponent& component : componentSet )
+    {
+        component.draw( *shader );
+    }
+    
+    glEnable( GL_DEPTH_TEST );
+    glDepthMask( true );
+
+    // unbind shader
+    shader->unbind();
+    // bind fbo
+    fbo->unbind();
+}
+
+
+void plArthroCamTechnique::_initState() const
+{
+    // enable back face culling
+    glEnable( GL_CULL_FACE );
+    glCullFace( GL_BACK );    
+    glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
+    // set depth testing
+    glEnable( GL_DEPTH_TEST ); 
+    glDepthFunc( GL_LEQUAL );
+    // enable blending
+    glEnable( GL_BLEND ); 
+    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );      
+}
